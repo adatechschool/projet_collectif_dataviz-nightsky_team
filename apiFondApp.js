8a8cba657907b990b56016091b38636b2b5e3914
JuanitaAK <afanadorjuanita@gmail.com>
2022-07-21 11:54:07 +0200

    doc a jour et fonctionnel

--- apiFondApp.js
+++ apiFondApp.js
@@ -1,29 +1,18 @@
-
-<<<<<<< HEAD
 const img = document.getElementById('img'); 
 const countryImage = [] ; 
-=======
-/* function searchBackground(){
->>>>>>> e21e7dcba01b315361a7fd2509678f34a7e0b496
+
 const apiKey = "dDVqjeRu2vBiQub8q4Ec5_qg79wZN7Ti4qL-rBNMs8c";
 const apiUrl = "https://api.unsplash.com/photos/?client_id="+apiKey; //?query=landscape&country searched
 
 
 // request to the API 
 
-   fetch(apiUrl)
-
+fetch(apiUrl)
    .then(res => res.json()) 
    .then((data) => {
-      console.log(img)
-      //img.src = data[1].urls.regular
       let body = document.getElementsByTagName('body')
-      document.body.style.backgroundImage = 'url("data[1].urls.regular")';
+      document.body.style.backgroundImage = 'url('+data[1].urls.regular+')';
    }) //  ici pour envoyer vers html  
     //query parameter ?"country"&landscape
    
  
-    
-
-
-
